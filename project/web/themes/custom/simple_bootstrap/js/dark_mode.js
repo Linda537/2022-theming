41b324bf21bb7528f505489268e9624cf84f7a84
Linda537 <lindasagetea@gmail.com>
2022-12-12 19:32:05 +0000

    done

--- project/web/themes/custom/simple_bootstrap/js/dark_mode.js
+++ project/web/themes/custom/simple_bootstrap/js/dark_mode.js
@@ -3,7 +3,10 @@
     Drupal.behaviors.dark_mode = {
         attach: function (context, settings){
             once('dark_mode', 'html', context).forEach(function (element){
-                alert("hello Tu");    
+                $('.dark-mode').click(function(){
+                    $('body').toggleClass('dark');
+
+                });   
             });
             
         }
